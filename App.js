f9cc8308a4dc7a7e83b4024fa80c06f975cf312a
phmiranda <pehhagah.1607@gmail.com>
2020-06-01 13:57:18 -0300

    [add] - modificação e criação das camadas necessárias utilizando expo.

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Olá, estamos iniciando o Instalura.</Text>
+      <Text>O Instalura é um clone do Instagram para estudo de React Native.</Text>
     </View>
   );
 }
